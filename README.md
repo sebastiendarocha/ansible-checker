f35b709ffb664ba9b5775b91c570e1943e3a3dfc
Sebastien DA ROCHA <sebastien@altsysnet.com>
2017-09-13 20:00:28 +0200

    Rist working version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ansible-checker
 A script to check if all playbooks would run OK
+
+## Install
+
+* Copy the files in your ansible.cfg directory (i.e. /etc/ansible/)
+* Move to your ansible dir.
+* Disable the "retry\_files\_enabled" parameter, it would break the script if a machine is unreachable or as an error.
+* Fill your ansible.yml setup file
+* Call the script and it will call all the playbooks in the specified environments.
+
+
+## Compatibility
+
+Tested with Ansible 2.3.2 and Python 3.4
---
+++ ansible-checks.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python3
+from subprocess import PIPE, Popen
+import json
+import yaml
+from pprint import pprint
+import os
+
+env = os.environ.copy()
+env['ANSIBLE_STDOUT_CALLBACK'] = 'json'
+
+with open("ansible-checks.yml", 'r') as stream:
+    data = yaml.load(stream)
+
+    for inventory in data:
+        environment = inventory["environment"]
+        playbooks = inventory["playbooks"]
+
+        for playbook in playbooks:
+            print("%s : %s" % (environment, playbook))
+            proc = Popen(
+                [
+                    "ansible-playbook", "-i", environment,
+                    playbook, '--flush-cache', "--check"
+                ],
+                env=env,
+                stdout=PIPE
+            )
+            json_output, _ = proc.communicate()
+            status = json.loads(json_output.decode())["stats"]
+
+            for host, values in status.items():
+                output = []
+                errors = values["unreachable"] + values["failures"]
+                changed = values["changed"]
+
+                if errors > 0:
+                    output.append("errors: %s" % errors)
+
+                if changed > 0:
+                    output.append("changes: %s" % changed)
+
+                if len(output) > 0:
+                    print("    %s : %s" % (host, ", ".join(output)))
