acf71733f0bf3381d836687167a6c88ae31fe4d8
Sebastien DA ROCHA <sebastien@da-rocha.net>
2017-11-11 13:38:39 +0100

    Added some error test on yaml output

--- test/test_yaml_output.py
+++ test/test_yaml_output.py
@@ -15,6 +15,41 @@ class TestAnsibleCheckYamlOutput(unittest.TestCase):
             pass
         utils.install_config_test_file(method.__name__)
 
+    def testErrorConfAbsentYaml(self):
+        try:
+            os.remove("ansible-checks.yml")
+        except OSError:
+            pass
+
+        with self.assertRaises(subprocess.CalledProcessError) as context:
+            subprocess.check_output('../ansible-checks.py',
+                                    stderr=subprocess.STDOUT)
+
+        self.assertTrue("returned non-zero exit status 1"
+                        in str(context.exception))
+
+    def testErrorInventoryYaml(self):
+
+        with self.assertRaises(subprocess.CalledProcessError) as context:
+            subprocess.check_output('../ansible-checks.py',
+                                    stderr=subprocess.STDOUT)
+
+        self.assertIn("returned non-zero exit status 2",
+                      str(context.exception))
+        self.assertIn("Path doesn't exists: hosts_fail",
+                      str(context.exception.output.decode()))
+
+    def testErrorPlaybookYaml(self):
+
+        with self.assertRaises(subprocess.CalledProcessError) as context:
+            subprocess.check_output('../ansible-checks.py',
+                                    stderr=subprocess.STDOUT)
+
+        self.assertIn("returned non-zero exit status 3",
+                      str(context.exception))
+        self.assertIn("Playbook doesn't exists: not_exists.yml",
+                      str(context.exception.output))
+
     def testConfChangedPlaybookYaml(self):
 
         output = subprocess.check_output('../ansible-checks.py',
